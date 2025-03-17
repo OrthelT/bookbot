a6536f9c100a8a0e7c29c6e8a8434b1bbc110219
Orthel Toralen <orthel.toralen@gmail.com>
2025-03-17 15:52:19 -0400

    added command line argument for book filepath

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import get_num_words, get_chars
 
 def get_book_text(filepath):
@@ -6,21 +7,25 @@ def get_book_text(filepath):
         return book_text
 
 def main():
-    book = get_book_text('books/frankenstein.txt')
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_file = sys.argv[1]
+    book = get_book_text(book_file)
     words = get_num_words(book) 
     message = f"Found {words} total words"
     dict_list = get_chars(book)
-    make_report(book,dict_list,message)
+    make_report(book_file,dict_list,message)
 
 def show_counts(dict_list):
     for d in dict_list:
         for k, v in d.items():
             print(f"{k}: {v}")
 
-def make_report(book_loc,dict_list, message):
+def make_report(book_file,dict_list, message):
     print(f"""
     ============ BOOKBOT ============
-    Analyzing book found at books/frankenstein.txt...
+    Analyzing book found at {book_file}...
   
     ----------- Word Count ----------
     {message}
